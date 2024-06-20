b27175a18792e0ca1666df7f1c5ddf031798f3df
Himanshu Kumar Saw <129617476+himanshu0ic7@users.noreply.github.com>
2024-06-20 17:08:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pandas_profiling
 import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
+from pydantic_settings import BaseSettings
 
 # Initialize session state for the dataframe
 if 'df' not in st.session_state:
